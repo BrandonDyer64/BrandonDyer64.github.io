332e4210f951f6c26e611499fa0e17107be82080
Brandon Dyer <brandondyer64@gmail.com>
2019-03-14 17:14:55 -0500

    New Portfolio

---
+++ src/projects.js
@@ -0,0 +1,59 @@
+export default [
+  {
+    name: "kepler",
+    title: "Kepler Engine",
+    img: require("./img/kepler.png"),
+    url: "https://keplerengine.com",
+    descriptionShort: "Game Engine"
+  },
+  {
+    name: "alvm",
+    title: "ALVM",
+    img: require("./img/alvm.png"),
+    url: "https://github.com/BrandonDyer64/ALVM",
+    descriptionShort: "AngelScript / Lua Virtual Machine"
+  },
+  {
+    name: "viav",
+    title: "Viav",
+    img: require("./img/viav.png"),
+    url: "https://viav.app",
+    descriptionShort: "Discord Bot"
+  },
+  {
+    name: "gitpromise",
+    title: "Git Promise",
+    url: "https://www.npmjs.com/package/promise-for-git",
+    img: require("./img/gitpromise.png"),
+    descriptionShort: "Convenience tool for Git repositories"
+  },
+  {
+    name: "weeds",
+    title: "Atmospheric Odyssey",
+    img: require("./img/space-invaders.jpg"),
+    descriptionShort: "Android mobile game"
+  },
+  {
+    name: "nnbricks",
+    title: "NNBricks",
+    img: require("./img/neural-network.png"),
+    attribution:
+      "Artificial Neural Network by sachin modgekar from the Noun Project",
+    url: "https://github.com/BrandonDyer64/nnbricks",
+    descriptionShort: "Simple self-playing video game using Machine Learning"
+  },
+  {
+    name: "zero",
+    title: "Zero Database",
+    img: require("./img/zero.png"),
+    url: "https://github.com/BrandonDyer64/Zero-Legacy",
+    descriptionShort: "Business management software"
+  },
+  {
+    name: "editor",
+    title: "Node Editor",
+    img: require("./img/editor.png"),
+    url: "https://github.com/BrandonDyer64/Editor",
+    descriptionShort: "Node based, graphical code editor"
+  }
+];
